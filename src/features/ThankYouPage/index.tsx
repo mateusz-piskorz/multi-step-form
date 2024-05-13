8027f374d56cd83832e2b503061f33e0bcfc7cf7
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-13 08:13:25 +0200

    ThankYouPage - update styles

--- src/features/ThankYouPage/index.tsx
+++ src/features/ThankYouPage/index.tsx
@@ -3,11 +3,16 @@ import thankYouIcon from "./assets/icon-thank-you.svg";
 import { styled, css } from "styled-components";
 
 export const ThankYouPage: FC = () => {
+  const { styledComponentId: ThankYouPage } = StyledThankYouPage;
   return (
-    <StyledThankYouPage>
-      <img src={thankYouIcon} alt="thank you icon" />
-      <h2>Thank you!</h2>
-      <p>
+    <StyledThankYouPage $className={ThankYouPage}>
+      <img
+        className={`${ThankYouPage}_icon`}
+        src={thankYouIcon}
+        alt="thank you icon"
+      />
+      <h2 className={`${ThankYouPage}_title`}>Thank you!</h2>
+      <p className={`${ThankYouPage}_description`}>
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com
@@ -16,48 +21,54 @@ export const ThankYouPage: FC = () => {
   );
 };
 
-const StyledThankYouPage = styled.div(({ theme }) => {
-  return css`
-    margin: auto;
-    padding: 50px 0 50px 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 15px;
-    text-align: center;
+const StyledThankYouPage = styled.div<{ $className: string }>(
+  ({ theme, $className }) => {
+    return css`
+      margin: auto;
+      padding: 50px 0 50px 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 15px;
+      text-align: center;
 
-    > img {
-      margin-bottom: 15px;
-      max-width: 65px;
-    }
+      .${$className} {
+        &_icon {
+          margin-bottom: 15px;
+          max-width: 65px;
+        }
 
-    > h2 {
-      color: ${theme.marineBlue};
-    }
+        &_icon {
+          color: ${theme.marineBlue};
+        }
 
-    > p {
-      color: ${theme.coolGray};
-      max-width: 290px;
-      line-height: 1.5rem;
-    }
-
-    @media screen and (min-width: 768px) {
-      > img {
-        max-width: unset;
+        &_description {
+          color: ${theme.coolGray};
+          max-width: 290px;
+          line-height: 1.5rem;
+        }
       }
-      > p {
-        font-size: 1rem;
-        max-width: 500px;
-      }
-    }
 
-    @media screen and (min-width: 1024px) {
-      > h2 {
-        font-size: 2rem;
+      @media screen and (min-width: 768px) {
+        .${$className} {
+          &_icon {
+            max-width: unset;
+          }
+
+          &_description {
+            font-size: 1rem;
+            max-width: 500px;
+          }
+        }
       }
-      > p {
-        font-size: 1rem;
+
+      @media screen and (min-width: 1024px) {
+        .${$className} {
+          &_title {
+            font-size: 2rem;
+          }
+        }
       }
-    }
-  `;
-});
+    `;
+  }
+);
