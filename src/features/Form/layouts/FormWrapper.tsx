7dd1614f60fe8c709ff35d26ffc58c19b0a0fc3c
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-14 16:15:52 +0200

    layouts/FormWrapper - update styles to BEM

--- src/features/Form/layouts/FormWrapper.tsx
+++ src/features/Form/layouts/FormWrapper.tsx
@@ -2,8 +2,8 @@ import { ReactNode, FC } from "react";
 import { styled, css } from "styled-components";
 
 type FormWrapperProps = {
-  title?: string;
-  description?: string;
+  title: string;
+  description: string;
   children?: ReactNode;
 };
 
@@ -12,65 +12,75 @@ export const FormWrapper: FC<FormWrapperProps> = ({
   description,
   children,
 }) => {
+  const { styledComponentId: FormWrapper } = StyledFormWrapper;
+
   return (
-    <Wrapper>
-      {title && <h2>{title}</h2>}
-      {description && <p>{description}</p>}
+    <StyledFormWrapper $className={FormWrapper}>
+      <h2 className={`${FormWrapper}_title`}>{title}</h2>
+      <p className={`${FormWrapper}_description`}>{description}</p>
       {children}
-    </Wrapper>
+    </StyledFormWrapper>
   );
 };
 
-const Wrapper = styled.div(({ theme }) => {
-  return css`
-    width: 90%;
-    max-width: 400px;
-    background-color: white;
-    border-radius: 15px;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    top: 100px;
-    padding: 30px 25px 30px 25px;
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    box-shadow: 0px 0px 9px rgba(0, 0, 0, 0.1);
+const StyledFormWrapper = styled.div<{ $className: string }>(
+  ({ theme, $className }) => {
+    return css`
+      width: 90%;
+      max-width: 400px;
+      background-color: white;
+      border-radius: 15px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: 100px;
+      padding: 30px 25px 30px 25px;
+      display: flex;
+      flex-direction: column;
+      gap: 15px;
+      box-shadow: 0px 0px 9px rgba(0, 0, 0, 0.1);
 
-    > h2 {
-      color: ${theme.marineBlue};
-    }
+      .${$className} {
+        &_title {
+          color: ${theme.marineBlue};
+        }
 
-    > p {
-      color: ${theme.coolGray};
-      margin-bottom: 20px;
-      max-width: 280px;
-      line-height: 1.4rem;
-    }
+        &_description {
+          color: ${theme.coolGray};
+          margin-bottom: 20px;
+          max-width: 280px;
+          line-height: 1.4rem;
+        }
+      }
 
-    @media screen and (min-width: 768px) {
-      gap: 5px;
-      max-width: unset;
-      position: relative;
-      top: unset;
-      left: unset;
-      transform: unset;
-      box-shadow: none;
-      width: auto;
-      > p {
+      @media screen and (min-width: 768px) {
+        gap: 5px;
         max-width: unset;
-      }
-    }
+        position: relative;
+        top: unset;
+        left: unset;
+        transform: unset;
+        box-shadow: none;
+        width: auto;
 
-    @media screen and (min-width: 1024px) {
-      gap: 10px;
-      > h2 {
-        font-size: 2rem;
+        .${$className} {
+          &_description {
+            max-width: unset;
+          }
+        }
       }
 
-      > p {
-        font-size: 1.1rem;
+      @media screen and (min-width: 1024px) {
+        gap: 10px;
+        .${$className} {
+          &_title {
+            font-size: 2rem;
+          }
+          &_description {
+            font-size: 1.1rem;
+          }
+        }
       }
-    }
-  `;
-});
+    `;
+  }
+);
