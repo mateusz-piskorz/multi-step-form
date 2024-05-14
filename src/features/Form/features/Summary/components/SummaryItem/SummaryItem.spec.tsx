c15943066652fa27df028d8fda735cdfc51365ca
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-14 08:50:02 +0200

    SummaryItem - update unit tests

--- src/features/Form/features/Summary/components/SummaryItem/SummaryItem.spec.tsx
+++ src/features/Form/features/Summary/components/SummaryItem/SummaryItem.spec.tsx
@@ -1,29 +1,58 @@
 import { render, screen } from "@testing-library/react";
 import { SummaryItem } from "./index";
+import { useForm } from "../../../../context";
 
 const defaultProps = {
-  cost: 3,
-  itemCase: "service",
-  displayedService: "customizableProfile",
-  period: "monthly",
-} as const;
+  cost: 150,
+  text: "textTest",
+};
 
-it("works", () => {
-  expect(2).toBe(2);
+const CostProps = jest.fn();
+jest.mock("../../../Cost", () => ({
+  Cost: jest.fn((props) => CostProps(props)),
+}));
+
+it("displays Cost", () => {
+  render(<SummaryItem {...defaultProps} />);
+  expect(CostProps).toHaveBeenCalledWith({
+    bold: undefined,
+    cost: 150,
+    gray: true,
+    period: "monthly",
+  });
 });
-// it("displays price", () => {
-//   render(<SummaryItem {...defaultProps} />);
-//   expect(screen.getByText(`$${defaultProps.cost}/mo`)).toBeInTheDocument();
-// });
 
-// it("displays service name", () => {
-//   render(<SummaryItem {...defaultProps} />);
-//   expect(screen.getByText(defaultProps.displayedService)).toBeInTheDocument();
-// });
+it("displays Cost bold", () => {
+  render(<SummaryItem {...defaultProps} boldCost />);
+  expect(CostProps).toHaveBeenCalledWith({
+    bold: true,
+    cost: 150,
+    gray: false,
+    period: "monthly",
+  });
+});
 
-// it("displays monthly text", () => {
-//   render(
-//     <SummaryItem {...defaultProps} itemCase="heading" selectedPlan="advanced" />
-//   );
-//   expect(screen.getByText("Advanced (Monthly)")).toBeInTheDocument();
-// });
+it("displays Cost yearly", () => {
+  (useForm as jest.Mock<any>).mockReturnValue({ watch: () => "yearly" });
+  render(<SummaryItem {...defaultProps} />);
+  expect(CostProps).toHaveBeenCalledWith({
+    bold: undefined,
+    cost: 150,
+    gray: true,
+    period: "yearly",
+  });
+});
+
+it("displays text", () => {
+  render(<SummaryItem {...defaultProps} />);
+  expect(screen.getByText(defaultProps.text)).toBeInTheDocument();
+});
+
+it("displays Change button", () => {
+  const backToPlanSelection = jest.fn();
+  render(
+    <SummaryItem {...defaultProps} backToPlanSelection={backToPlanSelection} />
+  );
+  screen.getByRole("button", { name: "Change" }).click();
+  expect(backToPlanSelection).toHaveBeenCalled();
+});
