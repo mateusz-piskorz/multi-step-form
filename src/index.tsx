f4135c821991284c237b60f0ae9a0f47b7d7cabc
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-01 19:50:39 +0200

    add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, myTheme } from "./styles";
-
+import { Analytics } from "@vercel/analytics/react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -12,6 +12,7 @@ root.render(
     <ThemeProvider theme={myTheme}>
       <GlobalStyle />
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
